40cf64a352257fbd5b6d9fc6a83ebb051b88277d
Ernest Low <ernestlow32@gmail.com>
2024-07-26 01:19:04 +0800

    re-enabled default route to make checking if app works on vercel easier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import { config } from "../config/config";
 import prisma from "../prisma/db/prisma";
 import {
@@ -38,15 +38,15 @@ const main = async () => {
   // });
   // console.dir(owners);
 
-  // app.get("/", (req: Request, res: Response) => {
-  //   res.send("Express + TypeScript Server");
-  // });
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
+  });
 
   app.post("/api/register", RegisterController); // ? Send Whole owner profile minus ownerMatches (don't save this in), return whole owner profile
   app.post("/api/login", LoginController); // ? Send username password, return whole ownerprofile, and JWT save into localstorage
 
-  // POST   /api/ownerprofile     -> Protected. Send JWT in body. Checks JWT, use corresponding email from JWT to return whole ownerprofile
-  // POST   /api/editprofile      -> Protected. Send whole owner profileEdit profile    ( all fields, except ownerMatches). Returns full ownerprofile that was saved. +ownermatches
+  // POST   /api/owner-profile     -> Protected. Send JWT in body. Checks JWT, use corresponding email from JWT to return whole ownerprofile
+  // POST   /api/edit-profile      -> Protected. Send whole owner profileEdit profile    ( all fields, except ownerMatches). Returns full ownerprofile that was saved. +ownermatches
   // GET    /api/view-pet         -> Return a number of owner profiles                  ( petname, petGender, petAge, areaLocation, petPicture[0] )
   // GET    /api/view-pet:id      -> Return corresponding owner ID's (public profile) - ( petName, petGender, petAge, areaLocation, petPicture[], petDescription )
 
