9ae61c91cbce04c8e0baed750cfc8f7c66e029e9
Ernest-Low <ernestlow32@gmail.com>
2024-07-27 11:00:54 +0800

    add netlify cors

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://dexsjw.github.io/react-pet-pal/",
   "https://dexsjw.github.io/",
+  "https://66a462864a43fca2280e7048--resonant-lily-81c514.netlify.app/",
 ];
 
 export default allowedOrigins;
