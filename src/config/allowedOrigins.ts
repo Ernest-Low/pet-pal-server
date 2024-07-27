7e61887c44ef9bd7a594a664f889839f3ff6e8e2
Ernest-Low <ernestlow32@gmail.com>
2024-07-27 10:13:39 +0800

    added cors allowedorigins of client hosted github pages

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   //* Add deployment website url when deploying
   "http://localhost:5173",
+  "https://dexsjw.github.io/react-pet-pal/",
+  "https://dexsjw.github.io/",
 ];
 
 export default allowedOrigins;
